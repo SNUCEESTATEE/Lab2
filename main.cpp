cbb390c7e31605fb566db5fe5169b33c64927108
SNUCEESTATEE <168407409+SNUCEESTATEE@users.noreply.github.com>
2024-06-07 16:54:03 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,16 +4,27 @@
 using namespace std;
 
 int N,M;
-vector<int> vec;
+vector<int> vec(10);
+vector<bool> visited(10);
 
 void func(int cnt) {
-
-
-
-
-
-
-
+	if(cnt == M) {
+		for(int i=0; i<M; i++) {
+			cout << vec[i] << " ";
+		}
+		cout << endl;
+      return;
+	}
+	else {
+		for(int i=1; i<=N; i++) {
+			if(!visited[i]) {
+				visited[i] = true;
+				vec[cnt] = i;
+				func(cnt + 1);
+				visited[i] = false;
+			}
+		}
+	}	
 }
 
 int main() {
